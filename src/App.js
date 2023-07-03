c55d45a68193f37c96730ea2e189091ef6beef2c
Kawaljeet Singh <kawaljeet.singh@minditsystems.com>
2023-07-03 16:42:42 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Employee from "./EmployeeComponent/Employee";
 
-function App() {
+const App = () => {
   return (
-   <>
-<div>Hello</div>
-   </>
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* {Login page routes} */}
+          <Route path="/login" element={<Employee />}>
+
+            {/* {SignUp Routes} */}
+
+            <Route path="/signup" element={<Employee />}>
+              <Route path="/auth-two-step" element={<Employee />}>
+                <Route path="/login" element={"<Employee />"} />
+              </Route>
+            </Route>
+
+            {/* {Forgot Password Routes} */}
+            <Route path="/forgotpassword" element={<Employee />}>
+              <Route path="/auth-two-step" element={<Employee />}>
+                <Route path="/login" element={"<Employee />"} />
+              </Route>
+            </Route>
+           
+  
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/EmployeeComponent/Employee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Employee=()=>{
+    return(
+        <>
+        <h1>hello</h1>
+        </>
+    );
+}
+
+export default Employee;
