0e01185876f05c15922b64645776d44518dded26
shubhi <shubhi.sharma@minditsystems.com>
2023-07-05 10:37:30 +0530

    manager code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import PageRouting from "./Routes/PageRouting";
 
 
+
 function App() {
   return (
    <>
-   <PageRouting/>
+  <PageRouting/>
    
    </>
   );
--- src/Routes/PageRouting.jsx
+++ src/Routes/PageRouting.jsx
@@ -5,8 +5,14 @@ import Login from "../Login/Login";
 import ForgotPassword from "../Login/ForgotPassword";
 
 import Auto_two_step from "../Login/Auto_two_step";
-import{BrowserRouter,Routes,Route}from "react-router-dom";
+import{Routes,Route}from "react-router-dom";
 import MainContent from "../Component/MainContent";
+import ManagerDashboard from "../ManagerComponent/ManagerDashboard";
+import ManagerLeave from "../ManagerComponent/ManagerLeave";
+import ManagerAttendance from "../ManagerComponent/ManagerAttendance";
+import ManagerExpense from "../ManagerComponent/ManagerExpense";
+import ManagerAlert from "../ManagerComponent/ManagerAlert";
+
 
 const PageRouting =()=>{
     const design =(
@@ -18,6 +24,12 @@ const PageRouting =()=>{
       <Route path="/forgotpassword" component={ForgotPassword}/>
       <Route path="/auto_two_step" component={Auto_two_step}/>
       <Route path="/auto_two_step#" component={Auto_two_step}/>
+      <Route path="/managerdashboard" component={ManagerDashboard}/>
+      <Route path="/managerleave" component={ManagerLeave}/>
+      <Route path="/managerattendance" component={ManagerAttendance}/>
+      <Route path="/managerexpense" component={ManagerExpense}/>
+      <Route path="/manageralert" component={ManagerAlert}/>
+
       </Routes>
       </>
     );
